2c2e036c5d2d20776d02aa2f6fb0b0b6c829d582
Amar Zavery <amzavery@microsoft.com>
2018-12-13 11:24:54 +0530

    semaphore updates

--- lib/util/semaphore.ts
+++ lib/util/semaphore.ts
@@ -7,18 +7,18 @@
  */
 export class Semaphore {
   /**
-   * The number od concurrent calls that can be made.
+   * The number of concurrent calls that can be made.
    */
   limit: number;
   private _queue: Array<(value?: void | PromiseLike<void> | undefined) => void> = [];
   private _used: number = 0;
   constructor(limit: number) {
-    if (typeof limit !== 'number') {
+    if (typeof limit !== "number") {
       throw new TypeError(`Expected limit to be a number, got ${typeof limit}`);
     }
 
     if (limit < 1) {
-      throw new Error('limit cannot be less than 1');
+      throw new Error("limit cannot be less than 1");
     }
     this.limit = limit;
   }
@@ -61,8 +61,27 @@ export class Semaphore {
   use<T>(fn: () => T | PromiseLike<T>): Promise<T> {
     return this.acquire()
       .then(fn)
-      .then(
-        (val) => { this.release(); return val; })
-      .catch((err) => { this.release(); throw err; });
+      .then((val) => {
+        this.release();
+        return val;
+      })
+      .catch((err) => {
+        this.release();
+        throw err;
+      });
+  }
+
+  /**
+   * Provides the number of locks currently held.
+   */
+  currentLockCount(): number {
+    return this._used;
+  }
+
+  /**
+   * Provides the number of tasks waiting to acquire a lock.
+   */
+  awaitedTaskCount(): number {
+    return this._queue.length;
   }
 }
