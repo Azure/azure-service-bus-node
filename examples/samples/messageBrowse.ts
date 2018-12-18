311b61c33b95b4f66c932e1fd2c9ea42e0435f6c
Sai Harsha Nalluru <10452642+HarshaNalluru@users.noreply.github.com>
2018-12-18 13:49:26 -0800

    ported messageBrowse sample to typescript

--- examples/samples/messageBrowse.ts
+++ examples/samples/messageBrowse.ts
@@ -1,145 +1,125 @@
-//   
-//   Copyright © Microsoft Corporation, All Rights Reserved
-// 
-//   Licensed under the Apache License, Version 2.0 (the "License"); 
-//   you may not use this file except in compliance with the License. 
-//   You may obtain a copy of the License at
-// 
-//   http://www.apache.org/licenses/LICENSE-2.0 
-// 
-//   THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
-//   OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
-//   ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A
-//   PARTICULAR PURPOSE, MERCHANTABILITY OR NON-INFRINGEMENT.
-// 
-//   See the Apache License, Version 2.0 for the specific language
-//   governing permissions and limitations under the License. 
+import {
+  OnMessage,
+  OnError,
+  MessagingError,
+  delay,
+  ServiceBusMessage,
+  ReceiveMode,
+  generateUuid,
+  Namespace
+} from "../../lib";
+import * as dotenv from "dotenv";
+dotenv.config();
 
-namespace MessageBrowse
-{
-    using Microsoft.Azure.ServiceBus;
-    using Microsoft.Azure.ServiceBus.Core;
-    using Newtonsoft.Json;
-    using System;
-    using System.Text;
-    using System.Threading.Tasks;
+const str = process.env.SERVICEBUS_CONNECTION_STRING || "";
+const path = process.env.QUEUE_NAME || "";
 
-    public class Program : MessagingSamples.Sample
-    {
-        public async Task Run(string connectionString)
-        {
-            // send a set of messages
-            await this.SendMessagesAsync(connectionString, BasicQueueName);
-            // broswe those messages 
-            await this.PeekMessagesAsync(connectionString, BasicQueueName);
-        }
+console.log("str: ", str);
+console.log("path: ", path);
 
-        async Task SendMessagesAsync(string connectionString, string queueName)
-        {
-            var sender = new MessageSender(connectionString,queueName);
-            
-            Console.WriteLine("Sending messages to Queue...");
-
-            dynamic data = new[]
-            {
-                new {name = "Einstein", firstName = "Albert"},
-                new {name = "Heisenberg", firstName = "Werner"},
-                new {name = "Curie", firstName = "Marie"},
-                new {name = "Hawking", firstName = "Steven"},
-                new {name = "Newton", firstName = "Isaac"},
-                new {name = "Bohr", firstName = "Niels"},
-                new {name = "Faraday", firstName = "Michael"},
-                new {name = "Galilei", firstName = "Galileo"},
-                new {name = "Kepler", firstName = "Johannes"},
-                new {name = "Kopernikus", firstName = "Nikolaus"}
-            };
-
-            // send a message for each entry in the above array
-            for (int i = 0; i < data.Length; i++)
-            {
-                var message = new Message(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(data[i])))
-                {
-                    ContentType = "application/json",
-                    Label = "Scientist",
-                    MessageId = i.ToString(),
-                    TimeToLive = TimeSpan.FromMinutes(2)
-                };
-
-                await sender.SendAsync(message);
-                lock (Console.Out)
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine("Message sent: Id = {0}", message.MessageId);
-                    Console.ResetColor();
-                }
-            }
-        }
-
-        async Task PeekMessagesAsync(string connectionString, string queueName)
-        {
-            var receiver = new MessageReceiver(connectionString, queueName, ReceiveMode.PeekLock);
-
-            Console.WriteLine("Browsing messages from Queue...");
-            while (true)
-            {
-                try
-                {
-                    // Browse messages from queue
-                    var message = await receiver.PeekAsync();
-                    // If the returned message value is null, we have reached the bottom of the log
-                    if (message != null)
-                    {
-                        // print the message
-                        var body = Encoding.UTF8.GetString(message.Body);
-                        lock (Console.Out)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Cyan;
-                            Console.WriteLine(
-                                "\t\t\t\tMessage peeked: \n\t\t\t\t\t\tMessageId = {0}, \n\t\t\t\t\t\tSequenceNumber = {1}, \n\t\t\t\t\t\tEnqueuedTimeUtc = {2}," +
-                                "\n\t\t\t\t\t\tExpiresAtUtc = {5}, \n\t\t\t\t\t\tContentType = \"{3}\", \n\t\t\t\t\t\tSize = {4}, \n\t\t\t\t\t\tState = {6}, "+
-                                "  \n\t\t\t\t\t\tContent: [ {7} ]",
-                                message.MessageId,
-                                message.SystemProperties.SequenceNumber,
-                                message.SystemProperties.EnqueuedTimeUtc,
-                                message.ContentType,
-                                message.Size,
-                                message.ExpiresAtUtc,
-                                "", //message.SystemProperties.State,// TODO: Need to restore that property
-                                body);
-                            Console.ResetColor();
-                        }
-                    }
-                    else
-                    {
-                        // We have reached the end of the log.
-                        break;
-                    }
-                }
-                catch (ServiceBusException e)
-                {
-                    if (!e.IsTransient)
-                    {
-                        Console.WriteLine(e.Message);
-                        throw;
-                    }
-                }
-            }
-            await receiver.CloseAsync();
-        }
-
-       public static int Main(string[] args)
-        {
-            try
-            {
-                var app = new Program();
-                app.RunSample(args, app.Run);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-                return 1;
-            }
-            return 0;
-        }
-    }
+async function main(): Promise<void> {
+  // send a set of messages
+  await sendMessage();
+  // browse those messages
+  await peekMessage();
+  // receive the messages [ to clear the queue ]
+  await receiveMessage();
 }
+
+async function sendMessage(): Promise<void> {
+  const nsSend = Namespace.createFromConnectionString(str);
+  const sendClient = nsSend.createQueueClient(path);
+  var data = [
+    { name: "Einstein", firstName: "Albert" },
+    { name: "Heisenberg", firstName: "Werner" },
+    { name: "Curie", firstName: "Marie" },
+    { name: "Hawking", firstName: "Steven" },
+    { name: "Newton", firstName: "Isaac" },
+    { name: "Bohr", firstName: "Niels" },
+    { name: "Faraday", firstName: "Michael" },
+    { name: "Galilei", firstName: "Galileo" },
+    { name: "Kepler", firstName: "Johannes" },
+    { name: "Kopernikus", firstName: "Nikolaus" }
+  ];
+  try {
+    for (let i = 0; i < data.length; i++) {
+      var message = {
+        body: JSON.stringify(data[i]),
+        contentType: "application/json",
+        label: "Scientist",
+        messageId: generateUuid()
+      };
+      await sendClient.send(message);
+      console.log("Sent message number:", i + 1);
+    }
+    console.log("\n>>>>>>> Total Sent messages: %d\n", data.length);
+  } catch (err) {
+    console.log("Error while sending", err);
+  }
+  return nsSend.close();
+}
+
+async function peekMessage(): Promise<void> {
+  const nsRcv = Namespace.createFromConnectionString(str);
+  const receiveClient = nsRcv.createQueueClient(path, { receiveMode: ReceiveMode.peekLock });
+  console.log("Browsing messages from Queue...");
+
+  try {
+    const count: number = 10;
+    //peek function takes "the number of messages to retrieve" as the argument
+    //If no arguments are passed to peek(), peeks the first message in the queue [Default value `1`]
+    const peekedMessage = await receiveClient.peek(count);
+    for (let i = 0; i < count; i++) {
+      console.log(
+        `Peeked message ${i}:   
+          ID - ${peekedMessage[i].messageId} , 
+          messageBody - ${peekedMessage[i].body}, 
+          label - ${peekedMessage[i].label}`
+      );
+    }
+    //Uncomment the following statement to check all the attributes of the peekedMessage[0]
+    //console.log(peekedMessage[0]);
+    console.log("\n>>>> Browsed the Messages!!!!\n");
+
+    await delay(1000);
+  } catch (err) {
+    console.log("Error while peeking: ", err);
+  }
+  return nsRcv.close();
+}
+
+async function receiveMessage(): Promise<void> {
+  const nsRcv = Namespace.createFromConnectionString(str);
+  const receiveClient = nsRcv.createQueueClient(path, { receiveMode: ReceiveMode.peekLock });
+  try {
+    const onMessage: OnMessage = async (brokeredMessage: ServiceBusMessage) => {
+      if (
+        brokeredMessage.label === "Scientist" &&
+        brokeredMessage.contentType === "application/json"
+      ) {
+        console.log(
+          "Message Received:",
+          brokeredMessage.body ? brokeredMessage.body.toString() : null
+        );
+      }
+      await brokeredMessage.complete();
+    };
+    const onError: OnError = (err: MessagingError | Error) => {
+      console.log(">>>>> Error occurred: ", err);
+    };
+    const rcvHandler = receiveClient.receive(onMessage, onError, { autoComplete: false });
+    await delay(10000);
+    await rcvHandler.stop();
+  } catch (err) {
+    console.log("Error while receiving: ", err);
+  }
+  return nsRcv.close();
+}
+
+main()
+  .then(() => {
+    console.log("\n>>>> Received the Messages!!!!");
+  })
+  .catch((err) => {
+    console.log("error: ", err);
+  });
