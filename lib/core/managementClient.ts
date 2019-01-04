2723fbd102c000c8db1071a40160b02446325c76
alexghiondea <ghiondea.alexandru@microsoft.com>
2019-01-04 12:12:10 -0800

    Update the flags passed to the compiler and fix the compiler warnings

--- lib/core/managementClient.ts
+++ lib/core/managementClient.ts
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
-import * as Long from "long";
+import Long from "long";
 import {
   EventContext,
   SenderEvents,
--- lib/serviceBusMessage.ts
+++ lib/serviceBusMessage.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-import * as Long from "long";
+import Long from "long";
 import {
   Delivery,
   uuid_to_string,
