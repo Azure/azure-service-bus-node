78a7b923546d1c4ae36dea17aa9589ac05357b50
HarshaNalluru <10452642+HarshaNalluru@users.noreply.github.com>
2019-01-22 12:02:59 -0800

    Fixed the bug related to receiveDeferredMessages

--- lib/subscriptionClient.ts
+++ lib/subscriptionClient.ts
@@ -266,16 +266,15 @@ export class SubscriptionClient extends Client {
     }
     return this._context.managementClient!.receiveDeferredMessage(sequenceNumber, this.receiveMode);
   }
-
   /**
    * Receives a list of deferred messages identified by `sequenceNumbers`.
    * @param sequenceNumbers A list containing the sequence numbers to receive.
-   * @returns Promise<ReceivedSBMessage[]>
+   * @returns Promise<ServiceBusMessage[]>
    * - Returns a list of messages identified by the given sequenceNumbers.
    * - Returns an empty list if no messages are found.
    * - Throws an error if the messages have not been deferred.
    */
-  async receiveDeferredMessages(sequenceNumbers: Long[]): Promise<ReceivedMessageInfo[]> {
+  async receiveDeferredMessages(sequenceNumbers: Long[]): Promise<ServiceBusMessage[]> {
     if (this.receiveMode !== ReceiveMode.peekLock) {
       throw new Error("The operation is only supported in 'PeekLock' receive mode.");
     }
